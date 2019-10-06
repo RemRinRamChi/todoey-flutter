419827eeb89b4f7d1d9b59d376794f063b59cfa2
Yao Jian Yap <yapyaojian731@gmail.com>
2019-10-06 21:39:09 +0800

    Used ListView.builder to list down the Tasks

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:todoey/models/task.dart';
 import 'package:todoey/widgets/task_tile.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(name: 'Buy Milk'),
+    Task(name: 'Buy Eggs'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        TaskTile(),
-        TaskTile(),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return TaskTile(
+          title: tasks[index].name,
+          isChecked: tasks[index].isDone,
+          checkboxCallback: (bool checkboxState) {
+            setState(() {
+              tasks[index].toggleDone();
+            });
+          },
+        );
+      },
+      itemCount: tasks.length,
     );
   }
 }
